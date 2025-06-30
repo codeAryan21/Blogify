7bb3a5e01860cce2a862f16bfb394b62d8805b0e
Aryan Singh <as5374329@gmail.com>
2025-06-30 18:28:30 +0530

    Index.js corrected

--- src/components/index.js
+++ src/components/index.js
@@ -4,7 +4,7 @@ import Container from "./Container/Container";
 import Logo from "./Logo";
 import LogoutBtn from "./Header/LogoutBtn";
 import Button from "./Button";
-import Input from "./input";
+import Input from "./Input";
 import Select from "./Select";
 import Login from "./Login";
 import Signup from "./SignUp";
